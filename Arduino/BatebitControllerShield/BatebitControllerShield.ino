6bc4c8ef4cbee2ceea2535517567f99efc841948
Filipe Calegario <filipe.calegario@gmail.com>
2020-12-02 18:52:39 -0300

    Correcting button pins

--- Arduino/BatebitControllerShield/BatebitControllerShield.ino
+++ Arduino/BatebitControllerShield/BatebitControllerShield.ino
@@ -1,15 +1,11 @@
 #include <MIDIUSB.h>
 #include <Bounce2.h>
 
-
-
 int pinosAnalogicos[3] = {A0, A1, A2};
 
-//int pinosBotoes[6] = {2, 4, 7, 8, 12, A5}; //versão 0.1 do Controller Shield
-//int pinosLeds[6] = {8, 9, 10, 11, 12, 13};
+int pinosBotoes[6] = {2, 3, 4, 5, 6, 7};
+int pinosLeds[6] = {8, 9, 10, 11, 12, 13};
 
-int pinosBotoes[6] = {2, 4, 7, 8, 12, A5};
-int pinosLeds[6] = {3, 5, 6, 9, 10, 11};
 int notasMIDI[6] = {40, 41, 42, 36, 37, 38}; //Padrão para Ableton
 int ccMIDI[3] = {16, 17, 18};
 
@@ -41,7 +37,7 @@ void loop() {
     int leituraAnalogicaMapeada = map(leituraAnalogicaAtual, 0, 1023, 0, 127);
     int delta = abs(leituraAnalogicaMapeada - ultimasLeiturasAnalogicas[i]);
     if (leituraAnalogicaMapeada != ultimasLeiturasAnalogicas[i]) {
-      controlChange(2, ccMIDI[i], leituraAnalogicaMapeada);
+      //controlChange(2, ccMIDI[i], leituraAnalogicaMapeada);
     }
     MidiUSB.flush();
     ultimasLeiturasAnalogicas[i] = leituraAnalogicaMapeada;
