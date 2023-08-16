4f2d211c97d94e09a9cb4771d5abdf6c3eb18489
João Tragtenberg <tragtenberg@gmail.com>
2023-08-16 17:49:36 -0300

    coloquei um filtro nos valores analógicos

--- Arduino/Controllerzinho/Controllerzinho.ino
+++ Arduino/Controllerzinho/Controllerzinho.ino
@@ -8,8 +8,11 @@ int pinosLeds[6] = {8, 9, 10, 11, 12, 13};
 
 int notasMIDI[6] = {40, 41, 42, 36, 37, 38}; //Padrão para Ableton
 int ccMIDI[3] = {16, 17, 18};
+int MidiChan = 3;
 
+int leituraAnalogicaAtual[3] = {0, 0, 0};
 int ultimasLeiturasAnalogicas[3] = {0, 0, 0};
+float leituraAnalogicaFiltrada[3] = {0.0, 0.0, 0.0};
 
 Bounce botao1 = Bounce();
 Bounce botao2 = Bounce();
@@ -33,11 +36,11 @@ void setup() {
 
 void loop() {
   for (int i = 0; i < 3; i++) {
-    int leituraAnalogicaAtual = analogRead(pinosAnalogicos[i]);
-    int leituraAnalogicaMapeada = map(leituraAnalogicaAtual, 0, 1023, 0, 127);
-    int delta = abs(leituraAnalogicaMapeada - ultimasLeiturasAnalogicas[i]);
+    leituraAnalogicaAtual[i] = analogRead(pinosAnalogicos[i]);
+    leituraAnalogicaFiltrada[i] = filtrar(leituraAnalogicaAtual[i], leituraAnalogicaFiltrada[i], 0.9);
+    int leituraAnalogicaMapeada = map(leituraAnalogicaFiltrada[i], 0, 1023, 0, 127);
     if (leituraAnalogicaMapeada != ultimasLeiturasAnalogicas[i]) {
-      //controlChange(2, ccMIDI[i], leituraAnalogicaMapeada);
+      controlChange(MidiChan - 1, ccMIDI[i], leituraAnalogicaMapeada);
     }
     MidiUSB.flush();
     ultimasLeiturasAnalogicas[i] = leituraAnalogicaMapeada;
@@ -49,14 +52,14 @@ void loop() {
       Serial.print("Apertou: botão ");
       Serial.println(i);
       digitalWrite(pinosLeds[i], HIGH);
-      noteOn(2, notasMIDI[i], 127);   // Channel 3, middle C, normal velocity
+      noteOn(MidiChan - 1, notasMIDI[i], 127);   // Channel 3, middle C, normal velocity
       MidiUSB.flush();
     }
     if (botoes[i]->fell()) {
       Serial.print("Levantou: botão ");
       Serial.println(i);
       digitalWrite(pinosLeds[i], LOW);
-      noteOff(2, notasMIDI[i], 0);   // Channel 3, middle C, normal velocity
+      noteOff(MidiChan - 1, notasMIDI[i], 0);   // Channel 3, middle C, normal velocity
       MidiUSB.flush();
     }
   }
@@ -75,4 +78,8 @@ void noteOff(byte channel, byte pitch, byte velocity) {
 void controlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
+}
+
+int filtrar(int valEntrada, int valFiltrado, float qtdeDeFiltragem) {
+  return ((1 - qtdeDeFiltragem) * valEntrada) + (qtdeDeFiltragem * valFiltrado);
 }
